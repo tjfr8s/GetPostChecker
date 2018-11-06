f1bd1d3e937b81998c10c54c6fb92f0528d2a054
Tyler Freitas <tjfr8s@gmail.com>
2018-11-06 11:49:30 -0800

    Initial commit.

---
+++ main.js
@@ -0,0 +1,52 @@
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.set('port', process.argv[2]);
+
+
+app.get('/', function(req, res){
+    var queryNames = [];
+    for(var name in req.query){
+        queryNames.push({"name": name, "value":req.query[name]});
+    }
+    var context = {};
+    context.data = queryNames;
+    res.render('get-request', context);
+});
+
+
+app.post('/', function(req, res){
+    var queryNames = [];
+    for(var name in req.body){
+        queryNames.push({"name": name, "value": req.body[name]});
+    }
+    var context = {};
+    context.data = queryNames;
+    res.render('post-request', context);
+});
+
+
+
+
+app.use(function (req, res) {
+    res.status(404);
+    res.render('404');
+});
+
+app.use(function (err, req, res, next){
+    console.error(err.stack);
+    res.type('text/plain');
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl + C  to terminate.');
+});
